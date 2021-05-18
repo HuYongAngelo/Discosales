f12bcd9968a0070d49f5db6ac8959bf974e897e4
hu.yongan <hu.yongan@TAVE-PC28.OSDBSESTO.local>
2021-05-18 12:03:59 +0200

    Fixed some comunication stuff

--- src/Server/ServerManaging.java
+++ src/Server/ServerManaging.java
@@ -6,28 +6,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ServerManaging implements Runnable {
     private Socket clientSocket;
     private ComunicationManager cm;
+    public static HashMap <Socket, String> hm;
+    private Utente u;
     
     public ServerManaging(Socket clientSocket) {
         this.clientSocket = clientSocket;
         cm = new ComunicationManager(clientSocket);
+        hm = new HashMap();
+    }
+    
+    public void riempiLista(String[] user) {
+        hm.put(clientSocket, user[1]);
     }
     
     @Override
     public void run() {
         try {
+            String richiesta;
             System.out.println("Serverino  partito: "+ clientSocket.getInetAddress());
             
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            riempiLista(in.readLine().split(";"));
             
-            String richiesta = in.readLine();
-            cm.Comunication(richiesta);
+            do {
+                richiesta = in.readLine();
+                cm.Comunication(richiesta);
+            } while(!richiesta.startsWith("exit"));
             
             out.close();
         } catch (IOException ex) {
