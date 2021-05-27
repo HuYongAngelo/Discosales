d99049b57683253f5584db073adb85e8dcbd6d3e
hu.yongan <hu.yongan@TAVE-PC28.OSDBSESTO.local>
2021-05-27 13:51:33 +0200

    Fixed file saving Needs some more features (probably)

--- src/Server/FileManager.java
+++ src/Server/FileManager.java
@@ -1,42 +1,81 @@
 package Server;
 
+import static Server.Server.hm;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class FileManager {
     private BufferedWriter bw;
     private BufferedReader br;
-    // Se è true, si sono create le cartelle;
-    // Se è false, esiste già la cartella con quella mail
-    private boolean controllo = true;
     
     public FileManager() {
+        createFiles();
     }
     
-    public void createFiles(String email) {
+    public void createFiles() {
         if (!new File("C:\\discosales").exists()) {
             new File("C:\\discosales").mkdir();
             new File("C:\\discosales\\Users").mkdir();
         }
     }
     
-    public void fileUsers(String email) {
+    public boolean fileUsersRegister(String email, String username, String password, Socket s) {
+     // Se è true, si sono create le cartelle;
+     // Se è false, esiste già la cartella con quella mail
+        boolean controllo = true;
         try {
-            bw = new BufferedWriter(new FileWriter("C:\\discosales\\Users\\"+email+".txt", true));
+            bw = new BufferedWriter(new FileWriter("C:\\discosales\\Users\\"+username+".txt", true));
             
-            if (!new File("C:\\discosales\\Users\\"+email+".txt").exists()) {
-                new File("C:\\discosales\\Users\\"+email+".txt").createNewFile();
+            if (!new File("C:\\discosales\\Users\\"+username+".txt").exists()) {
+                new File("C:\\discosales\\Users\\"+username+".txt").createNewFile();
+                hm.put(s, username);
                 controllo = true;
             } else {
-               controllo = false; 
+                controllo = false; 
             }
+            
+            bw.write(email+":"+username+":"+password);
         } catch (IOException ex) {
             Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return controllo;
+    }
+    
+    public boolean fileUsersLogin(String username, String password) {
+     // Se è true
+     // Se è false
+        boolean controllo = true;
+        String[] dati;
+        File f = new File("C:\\discosales\\Users\\"+username+".txt");
+        
+        if (f.exists()) {
+            try {
+                br = new BufferedReader(new FileReader(f));
+
+                dati = br.readLine().split(":");
+                
+                if (dati[2].equals(password)) {
+                    controllo = true;
+                } else {
+                    controllo = false;
+                }
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            controllo = false;
+        }
+        
+        return controllo;
     }
 }
