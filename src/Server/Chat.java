ab3a815383ea771c72399c77f48576613f2250bc
Hu Yong An <Hu Yong An@192.168.0.109>
2021-05-30 15:50:16 +0200

    Removed reading

--- src/Server/Chat.java
+++ src/Server/Chat.java
@@ -41,20 +41,6 @@ public class Chat {
     }
     
     public void reading(PrintWriter out) {
-        String messages = "";
-        try {
-            br = new BufferedReader(new FileReader("C:\\discosales\\chatLog.txt"));
-            
-            do {
-                messages = br.readLine();
-                if (messages != null) {
-                    out.println("read;"+messages);
-                }
-            } while(messages != null);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        out.println("read;"+messages);
     }
 }
